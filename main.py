b5e3952f71619d052ddbfb6882454b9c37d07f97
Budi Santoso <budi@jkt1.ebdesk.com>
2021-07-15 21:43:15 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from turtle import Screen, Turtle
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+screen.title("Python Snake Game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
